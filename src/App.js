10343c73f60401f08f4cb651e7dc9a9d87557e7d
lucarighes <luca.righes@gmail.com>
2021-10-22 09:34:03 -0700

    UI experiments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect } from 'react';
+import Layout from './components/layout';
 
 function App() {
 
@@ -15,24 +14,7 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello {test}</p>
-      </header>
-    </div>
+      <Layout>{test}</Layout>
   );
 }
 
