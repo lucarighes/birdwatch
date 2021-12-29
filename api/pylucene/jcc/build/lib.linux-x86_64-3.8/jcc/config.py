a458353b9f5cc4cb65e82256117389687f8cb554
lucarighes <luca.righes@gmail.com>
2021-12-29 11:07:37 -0800

    remove lucene

--- api/pylucene/jcc/build/lib.linux-x86_64-3.8/jcc/config.py
+++
@@ -1,8 +0,0 @@
-
-INCLUDES=['/usr/lib/jvm/java-8-oracle/include', '/usr/lib/jvm/java-8-oracle/include/linux']
-CFLAGS=['-fno-strict-aliasing', '-Wno-write-strings']
-DEBUG_CFLAGS=['-O0', '-g', '-DDEBUG']
-LFLAGS=['-L/usr/lib/jvm/java-8-oracle/jre/lib/amd64', '-ljava', '-L/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server', '-ljvm', '-Wl,-rpath=/usr/lib/jvm/java-8-oracle/jre/lib/amd64:/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server']
-IMPLIB_LFLAGS=[]
-SHARED=True
-VERSION="3.10"
